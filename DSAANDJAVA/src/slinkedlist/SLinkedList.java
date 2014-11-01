6cb8f4076c400bd1676787f2b80a406da17ccf37
deependerrathore <deepak.republic@gmail.com>
2014-11-01 20:52:11 +0530

    Partial implementation of the class for singly linked list

---
+++ DSAANDJAVA/src/slinkedlist/SLinkedList.java
@@ -0,0 +1,13 @@
+package slinkedlist;
+/**Single Linked List*/
+public class SLinkedList {
+	protected Node head; 		//head node of the list
+	protected long size;		//number of nodes in the list
+	/**Default constructor that creates an empty list*/
+	
+	public SLinkedList(){
+		head = null;
+		size = 0;
+	}
+	
+}
